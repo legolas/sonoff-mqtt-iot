9ed946366cf464926a90d9a9c96b0cdeb5db451e
Marcel Dullaart <marcel.dullaart@gmail.com>
2017-04-27 18:17:44 +0200

    Added On and Off button

--- home-automation/e2e/app.e2e-spec.ts
+++ home-automation/e2e/app.e2e-spec.ts
@@ -1,14 +1,23 @@
-import { HomeAutomationPage } from './app.po';
+import { HomeAutomationPage } from "./app.po";
+import { by, element } from 'protractor';
 
-describe('home-automation App', () => {
+describe("home-automation App", () => {
     let page: HomeAutomationPage;
 
     beforeEach(() => {
         page = new HomeAutomationPage();
     });
 
-    it('should display message saying app works', () => {
+    it("should display message saying app works", () => {
         page.navigateTo();
-        expect(page.getParagraphText()).toEqual('app works!');
+        expect(page.getParagraphText()).toEqual("app works!");
+    });
+
+    it("should display the 'On' button", () => {
+        element(by.buttonText("On")).isDisplayed();
+    });
+
+    it("should display the 'Off' button", () => {
+        element(by.buttonText("Off")).isDisplayed();
     });
 });
