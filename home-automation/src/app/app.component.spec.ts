47c272be5e9461298b8176188cf015e26f7cb7b0
Marcel Dullaart <marcel.dullaart@gmail.com>
2017-04-28 22:00:06 +0200

    Changed application title

--- home-automation/src/app/app.component.spec.ts
+++ home-automation/src/app/app.component.spec.ts
@@ -1,6 +1,10 @@
 import { TestBed, async } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HttpModule } from '@angular/http';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -8,6 +12,12 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+        imports: [
+            BrowserModule,
+            FormsModule,
+            HttpModule,
+            NgbModule.forRoot()
+        ]
     }).compileComponents();
   }));
 
@@ -17,16 +27,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
+  it(`should have as title 'Home Automation'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
+    expect(app.title).toEqual('Home Automation');
   }));
 
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('Home Automation');
   }));
 });
