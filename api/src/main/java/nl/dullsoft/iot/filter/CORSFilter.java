74c34d983db04515ce70f0bc52a4b2801d095713
Marcel Dullaart <marcel.dullaart@gmail.com>
2017-04-30 19:43:00 +0200

    Added CORS Header

---
+++ api/src/main/java/nl/dullsoft/iot/filter/CORSFilter.java
@@ -0,0 +1,43 @@
+package nl.dullsoft.iot.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author <a href="mailto:marcel.dullaart@rws.nl">Marcel Dullaart</a>
+ */
+public class CORSFilter implements Filter {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CORSFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        LOGGER.info("CORSFilter is initialised");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        setCorsHeaders((HttpServletRequest) servletRequest, (HttpServletResponse) servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        LOGGER.info("CORSFilter is destroyed");
+    }
+
+    private void setCorsHeaders(HttpServletRequest request, HttpServletResponse response) {
+
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setHeader("Access-Control-Allow-Headers",
+                request.getHeader("Access-Control-Request-Headers"));
+        }
+    }
+}
---
+++ api/src/test/java/nl/dullsoft/iot/filter/CORSFilterTest.java
@@ -0,0 +1,84 @@
+package nl.dullsoft.iot.filter;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.mockito.Mockito.*;
+
+/**
+ * @author <a href="mailto:marcel.dullaart@rws.nl">Marcel Dullaart</a>
+ */
+public class CORSFilterTest {
+
+    private static final String ACCESS_CONTROL_REQUEST_HEADER = "Access-Control-Request-Headers";
+    private static final String REQUESTED_HEADER = "Requested-Header";
+    @Rule
+    public MockitoRule mockitoRule = MockitoJUnit.rule();
+    @Mock
+    private FilterConfig filterConfig;
+    @Mock
+    private HttpServletRequest request;
+    @Mock
+    private HttpServletResponse response;
+    @Mock
+    private FilterChain chain;
+
+    @InjectMocks
+    private CORSFilter corsFilter;
+
+    @Test
+    public void init_ShouldDoNothing() throws ServletException {
+        corsFilter.init(filterConfig);
+        verifyZeroInteractions(filterConfig);
+    }
+
+    @Test
+    public void destroy_ShouldDoNothing() {
+        corsFilter.destroy();
+        verifyZeroInteractions(filterConfig);
+    }
+
+    @Test
+    public void doFilterGet_ShouldAddCorsHeader() throws ServletException, IOException {
+        when(request.getMethod()).thenReturn("GET");
+        when(request.getHeader(ACCESS_CONTROL_REQUEST_HEADER)).thenReturn(REQUESTED_HEADER);
+
+        CORSFilter
+            filter = new CORSFilter();
+
+        filter.init(filterConfig);
+        filter.doFilter(request, response, chain);
+        filter.destroy();
+
+        verify(response).setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
+        verify(response).setHeader("Access-Control-Allow-Origin", "*");
+        verify(response, never()).setHeader("Access-Control-Allow-Headers", REQUESTED_HEADER);
+    }
+
+    @Test
+    public void doFilterOptions_ShouldAddCorsHeader() throws ServletException, IOException {
+        when(request.getMethod()).thenReturn("OPTIONS");
+        when(request.getHeader(ACCESS_CONTROL_REQUEST_HEADER)).thenReturn(REQUESTED_HEADER);
+
+        CORSFilter filter = new CORSFilter();
+
+        filter.init(filterConfig);
+        filter.doFilter(request, response, chain);
+        filter.destroy();
+
+        verify(response).setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS");
+        verify(response).setHeader("Access-Control-Allow-Origin", "*");
+        verify(response).setHeader("Access-Control-Allow-Headers", REQUESTED_HEADER);
+    }
+}
